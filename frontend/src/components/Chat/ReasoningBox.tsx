bedc94d0762aada3bab88d27f26ba0623c88d54c
lintanganugerah <lintanganugerah4@gmail.com>
2025-06-21 17:10:22 +0800

    Add toggle hidden on reasoning component

--- frontend/src/components/Chat/ReasoningBox.tsx
+++ frontend/src/components/Chat/ReasoningBox.tsx
@@ -1,14 +1,39 @@
+import { FaChevronDown } from "react-icons/fa";
 import { ReasoningBoxProps } from "../../types/ChatTypes";
+import { useState } from "react";
 
 export function ReasoningBox({ header, timeElapsed, body }: ReasoningBoxProps) {
+  const [isBodyVisible, setIsBodyVisible] = useState(false);
+
+  const toggleBody = () => {
+    setIsBodyVisible(!isBodyVisible);
+  };
   return (
     <div className="flex justify-start">
-      <div className="w-full p-4">
-        <div className="bg-zinc-950 font-bold p-4 flex gap-4 items-center">
-          <p>{header}</p>
-          <p className="text-sm font-light">{timeElapsed}s</p>
+      <div className="w-full">
+        <div
+          className="bg-zinc-950 font-bold p-4 flex items-center justify-between"
+          id="header"
+        >
+          <div className="flex flex-row items-center gap-4">
+            <p>{header}</p>
+            <p className="text-sm font-light">{timeElapsed}s</p>
+          </div>
+          <FaChevronDown
+            onClick={toggleBody}
+            className={`cursor-pointer transform transition-transform duration-500 ${
+              isBodyVisible ? "rotate-180" : ""
+            }`}
+          />
         </div>
-        <div className="bg-zinc-900 p-8 text-zinc-400">{body}</div>
+        {isBodyVisible && (
+          <div
+            className="bg-zinc-900 p-8 text-zinc-400 transition-all duration-500"
+            id="body"
+          >
+            {body}
+          </div>
+        )}
       </div>
     </div>
   );
